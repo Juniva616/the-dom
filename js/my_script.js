a0c865a68a64e6c31ed260d86f81d05af6909a41
Juniva616 <juniva616@gmail.com>
2022-11-26 16:06:24 +0000

    Add index.html

--- js/my_script.js
+++ js/my_script.js
@@ -6,15 +6,17 @@ $(document).ready(
 
 $( "p" ).first().click(function() {
     $( this ).fadeTo( "slow", 0.33 );
-});
+}); //this sample I took from inet to check if it works. no, It does not.
 
 $("#start-button").click(function(){ 
-    $('h1').addClass('color-red');
+    $('h1').addClass('color-red'); //this is my idea
 }); 
 
 $("#start-button").hover(function(){ 
       $('#start-button').addClass('bg-lightblue');
 }); 
+
+//below teacher's samples:
 
 $('#start-button').mouseenter(function(){ 
       $('question').fadeTo(1000,0.5);
